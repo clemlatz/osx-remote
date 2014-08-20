c0e424cd9894fc83b0213127f683f86fe1ded744
Clément Bourgoin <cb@nokto.net>
2014-08-20 21:15:28 +0200

    Import config from config.json & no password set error

--- server.js
+++ server.js
@@ -3,40 +3,23 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 var applescript = require('applescript');
 
-var server = {
-	"name": "My OSX-Remote Server",
-	"port": 80,
-	"modules": {
-		"computer": true,
-		"applescript": true
-	}
+var config = require('./config.json');
+
+if (typeof config.server.password === 'undefined' || config.server.password == '') 
+{
+	console.log('Error : you need to set a password in config.json. Exiting...');
+	process.exit(1);
 }
 
 // Client page
 app.get('/', function(req, res){
-	server.hostname = req.headers.host;
-  res.render(__dirname+'/client.ejs', {
-	  "server": server
-  });
-});
-
-// Socket server
-io.sockets.on('connection', function (socket) {
-	
-	socket.on('message', function (message) {
-	        console.log('Un client me parle ! Il me dit : ' + message);
-	    });
-	
-    console.log('User connected');
-	
-	    // Quand le serveur reçoit un signal de type "message" du client    
-	    socket.on('launch', function (apptolaunch) {
-	        console.log('User asked to launch ' + apptolaunch);
-	    });	
-		
+	config.server.hostname = req.headers.host;
+	res.render(__dirname+'/client.ejs', {
+	  	"config": config
+	});
 });
 
 // HTTP Server
-http.listen(server.port, function(){
-  console.log('Listening on port '+server.port);
+http.listen(config.server.port, function(){
+  	console.log('Listening on port '+config.server.port);
 });
